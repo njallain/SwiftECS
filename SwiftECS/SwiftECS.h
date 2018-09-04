c6c0cb90b68d56c9a21f46485b646526287dc18b
Neil Allain <njallain@bluerope.org>
2018-09-04 06:32:31 -0500

    Initial Commit

---
+++ SwiftECS/SwiftECS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftECS.h
+//  SwiftECS
+//
+//  Created by Neil Allain on 9/4/18.
+//  Copyright © 2018 Neil Allain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftECS.
+FOUNDATION_EXPORT double SwiftECSVersionNumber;
+
+//! Project version string for SwiftECS.
+FOUNDATION_EXPORT const unsigned char SwiftECSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftECS/PublicHeader.h>
+
+
